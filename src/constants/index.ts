cb5506a9b4c919413284164b4c8658734ace33d3
Michael Standen <mstan@horizon.io>
2024-08-21 14:34:05 +1200

    Add native token swap support

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,12 @@
 // sales params
 export const NFT_TOKEN_ADDRESS = '0xe330d543e9189450c36dc873aa3ab14106b1ee87'
 export const SALES_CONTRACT_ADDRESS = '0xfdd0d596350a78c3852a43d3b5910154b7c644db'
+export const FAUX_NATIVE_TOKEN_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
+export const NATIVE_TOKEN_DETAILS = {
+  name: 'Ether',
+  symbol: 'ETH'
+}
+export const WETH_CONTRACT_ADDRESS = '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14'
 export const CHAIN_ID = 11155111
 export const ETHERSCAN_URL = 'https://sepolia.etherscan.io'
 export const SWAP_CURRENCY = {
